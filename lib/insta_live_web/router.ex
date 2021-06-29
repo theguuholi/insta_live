1a245532a47dc6da1d011aac082c7d5284f286bb
Gustavo <g.92oliveira@gmail.com>
2021-06-29 07:04:09 -0300

    Install materialize

--- lib/insta_live_web/router.ex
+++ lib/insta_live_web/router.ex
@@ -20,21 +20,8 @@ defmodule InstaLiveWeb.Router do
   scope "/", InstaLiveWeb do
     pipe_through :browser
 
-    live "/", PageLive, :index
   end
 
-  # Other scopes may use custom stacks.
-  # scope "/api", InstaLiveWeb do
-  #   pipe_through :api
-  # end
-
-  # Enables LiveDashboard only for development
-  #
-  # If you want to use the LiveDashboard in production, you should put
-  # it behind authentication and allow only admins to access it.
-  # If your application does not have an admins-only section yet,
-  # you can use Plug.BasicAuth to set up some basic authentication
-  # as long as you are also using SSL (which you should anyway).
   if Mix.env() in [:dev, :test] do
     import Phoenix.LiveDashboard.Router
 
@@ -62,6 +49,7 @@ defmodule InstaLiveWeb.Router do
   scope "/", InstaLiveWeb do
     pipe_through [:browser, :require_authenticated_user]
 
+    live "/", PageLive, :index
     get "/users/settings", UserSettingsController, :edit
     put "/users/settings", UserSettingsController, :update
     get "/users/settings/confirm_email/:token", UserSettingsController, :confirm_email
